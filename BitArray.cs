ac6f888cbd38d00decadcbf72fc8b134eee118b1
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-13 12:15:16 +0200

    exit function

--- BitArray.cs
+++ BitArray.cs
@@ -6,38 +6,49 @@ class Program
     static void Main()
     {
         int someValue, bitValue, numbersOfBits;
+        string input;
         List<bool> bitArray = new List<bool>();
 
-        someValue = Convert.ToInt32(Console.ReadLine());
-        numbersOfBits = (int) (Math.Log(someValue)/Math.Log(2)); //Calc the exponet of 2, casted to an int to be used as the smallest numbers of bits needed.
-        Console.WriteLine(numbersOfBits);
+        input = Console.ReadLine();
+        bool exit = (input == "exit" ? exit = true : exit = false);
 
-        for (int i = numbersOfBits; i >= 0; i--)
+        while(!exit)
         {
-            bitValue = (int) Math.Pow(2, i);
+            someValue = Convert.ToInt32(input);
+            numbersOfBits = (int) (Math.Log(someValue)/Math.Log(2)); //Calc the exponet of 2, casted to an int to be used as the smallest numbers of bits needed.
+            Console.WriteLine(numbersOfBits);
 
-            if ((someValue / bitValue) >= 1) // When someValue can be divided by a given bitValue, we know this bit must be 1
+            for (int i = numbersOfBits; i >= 0; i--)
             {
-                someValue = someValue - bitValue; // 
-                bitArray.Add(true);
-            }
-            else
-            {
-                bitArray.Add(false);
-            }
-        }
-        foreach (bool bit in bitArray)
-        {
-            switch (bit) //something
-            {
-                case true:
-                    Console.Write("1 ");
-                break;
+                bitValue = (int) Math.Pow(2, i);
 
-                case false:
-                    Console.Write("0 ");
-                break;
+                if ((someValue / bitValue) >= 1) // When someValue can be divided by a given bitValue, we know this bit must be 1
+                {
+                    bitArray.Add(true);
+                    someValue = someValue - bitValue;
+                }
+                else
+                {
+                    bitArray.Add(false);
+                }
             }
+            foreach (bool bit in bitArray)
+            {
+                switch (bit)
+                {
+                    case true:
+                        Console.Write("1 ");
+                    break;
+
+                    case false:
+                        Console.Write("0 ");
+                    break;
+                }
+            }
+            Console.WriteLine("");
+
+            input = Console.ReadLine();
+            exit = (input == "exit" ? exit = true : exit = false);
         }
     }
 }

9ccff3466818dea80085efa74ae8594c01baf9ee
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-20 12:22:08 +0200

    added funktion bergner

---
+++ matFunktioner/Program.cs
@@ -0,0 +1,69 @@
+﻿
+class Mat
+{
+    public double pow(double a, int n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            a = a * n;
+        }
+
+        return a;
+    }
+}
+
+class Function
+{
+    public double a, b, c, d;
+    
+
+    public Function (double aInput)
+    {
+        a = aInput;
+    }
+    public Function (double aInput, double bInput)
+    {
+        a = aInput;
+        b = bInput;
+    }
+    public Function (double aInput, double bInput, double cInput)
+    {
+        a = aInput;
+        b = bInput;
+        c = cInput;
+    }
+    public Function (double aInput, double bInput, double cInput, double dInput)
+    {
+        a = aInput;
+        b = bInput;
+        c = cInput;
+        d = dInput;
+    }
+
+    public double Value(double xInput)
+    {
+        Mat mat = new Mat();
+        double y;
+        double x = xInput;
+
+        y = (d*mat.pow(x, 3)) + (c * mat.pow(x, 2)) + (b * x) + a;
+
+        return y;
+    }
+
+    public char[,] Plot (int xRange, int yRange)
+    {
+        char[,] plot = new char[xRange, yRange];
+
+        return plot;
+    }
+}
+
+class program()
+{
+    static void Main()
+    {
+        Function newFuction = new Function(2, 2, 2, 2);
+        Console.WriteLine(newFuction.Value(10));
+    }
+}
---
+++ noiseMap/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        int[,] map = new int[10, 10];
+
+        for (int row = 0; row < 10; row++)
+        {
+            for (int column = 0; column < 10; column++)
+            {
+                map[row, column] = rand.Next(0, 10);
+            }
+        }
+
+        for (int row = 0; row < 10; row++)
+        {
+            Console.WriteLine();
+            
+            for (int column = 0; column < 10; column++)
+            {
+                Console.Write(map[row, column] + " ");
+            }
+        }
+
+    }
+}
